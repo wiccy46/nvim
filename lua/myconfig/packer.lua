989821f18ed6da702f9eae9c5b7302c0cc100b5c
Jiajun <eric1986_chn@yahoo.com>
2023-07-25 22:20:44 +0200

    Move to Lua

---
+++ lua/myconfig/packer.lua
@@ -0,0 +1,48 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use({
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+
+	})
+	use({ 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} })
+	use('ThePrimeagen/harpoon')
+	use('mbbill/undotree')
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			'williamboman/mason.nvim',
+			run = function()
+				pcall(vim.api.nvim_command, 'MasonUpdate')
+			end,
+			},
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Require
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		}
+	}
+	use('williamboman/mason.nvim')
+    use('github/copilot.vim')
+    use('tpope/vim-commentary')
+	use('vim-airline/vim-airline')
+	use('vim-airline/vim-airline-themes')
+    use('preservim/nerdtree')
+    use('liuchengxu/vim-which-key')
+end)
+
