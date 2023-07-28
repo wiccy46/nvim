eed3049a3dfd6fbe8bb4626dca64a1cbac5edcb4
Jiajun <eric1986_chn@yahoo.com>
2023-07-28 17:54:46 +0200

    Add more lua key bind

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,3 +10,20 @@ end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  }
+})
+
+-- local cmp = require('cmp')
+-- local cmp_action = require('lsp-zero').cmp_action()
+
+-- cmp.setup({
+--   mapping = {
+--     ['<Tab>'] = cmp_action.tab_complete(),
+--     ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+--   }
+-- })
