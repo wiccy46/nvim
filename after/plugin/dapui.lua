9b7f54a0035dc6f08735bdbadc8ec6a86244c714
Jiajun <eric1986_chn@yahoo.com>
2023-11-24 23:27:19 +0100

    Dap ui keys map

--- after/plugin/dapui.lua
+++ after/plugin/dapui.lua
@@ -4,18 +4,30 @@ require("dap-go").setup()
 require("dap-python").setup()
 
 -- Set keymaps to control the debugger
--- vim.keymap.set('n', '<F5>', require 'dap'.continue)
 vim.keymap.set('n', '<F10>', require 'dap'.step_over)
 vim.keymap.set('n', '<F11>', require 'dap'.step_into)
 vim.keymap.set('n', '<F12>', require 'dap'.step_out)
--- vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
+vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
 
 
 vim.api.nvim_set_keymap("n", "<leader>dt", ":DapUiToggle<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>do", ":lua require('dapui').open()<CR>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>dq", ":lua require('dapui').close()<CR>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", {noremap = true})
 
+-- Initialize a global variable
+_G.dapui_state = _G.dapui_state or false
+
+function ToggleDapUI()
+    local dapui = require('dapui')
+    if _G.dapui_state then
+        dapui.close()
+        _G.dapui_state = false
+    else
+        dapui.open()
+        _G.dapui_state = true
+    end
+end
+
+vim.api.nvim_set_keymap("n", "<leader>do", ":lua ToggleDapUI()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", {noremap = true})
 vim.fn.sign_define('DapBreakpoint', {text='🛑', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint'})
