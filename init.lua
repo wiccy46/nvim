a2d62619e0f61f2e433a9da649977e25c12753b3
Jiajun <eric1986_chn@yahoo.com>
2024-10-11 10:23:41 +0200

    Add .Jenkinsfile to be groovy

--- init.lua
+++ init.lua
@@ -6,3 +6,11 @@ vim.filetype.add({
     Jenkinsfile = "groovy",
   },
 })
+
+
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+    pattern = "*.Jenkinsfile",
+    callback = function()
+        vim.bo.filetype = "groovy"
+    end,
+})
