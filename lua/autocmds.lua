99da7ca2126e91fc9415a54475751261fc535364
wiccy46 <eric1986_chn@yahoo.com>
2026-01-19 18:26:06 +0100

    cpp tab to 4

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,12 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("FileType", {
+  pattern = { "c", "cpp", "objc", "objcpp", "cuda" },
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
